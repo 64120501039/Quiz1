98f8b23b7ddc264ef071a63d0a12a95c9a967e01
pairaya nuchpitak <111561499+64120501039@users.noreply.github.com>
2022-09-16 16:24:10 +0700

    1039 Quiz1

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿public class program {
+    static ReadingRoom data;
+    static void Main(){
+        data = new ReadingRoom();
+        Hairpin();
+    }
+        static void Hairpin(){
+            Console.WriteLine("Input menu");
+            Console.WriteLine("a = University");
+            Console.WriteLine("b = Academia");
+            Console.WriteLine("c = Electricity FT");
+            string input = Console.ReadLine();
+            if(input == "a" ) data.University();
+            if(input == "d" ) data.Academia();
+            if(input == "c" ) data.ElectricityFT();
+
+
+
+
+        }
+
+        }
+
+
+
---
+++ ReadingRoom.cs
@@ -0,0 +1,27 @@
+public class ReadingRoom {
+    public  ReadingRoom(){}
+    public void University(){
+        Console.WriteLine("college name : real name : inhabited : number phone");
+        Console.WriteLine("CU:RakDi: Pathumwan District, Bangkok : 02-215-3555");
+        Console.WriteLine("MU:Rak Lok : Salaya District Nakhon Pathom : 02-849-6211");
+        Console.WriteLine("KMITL :Ke Ki :Lat Krabang MarketBangkok : 02-329-8000");
+        Console.WriteLine("KKU :Watthana:district in the city of the essence: 043-002539 ");
+        Console.WriteLine("KMUTT :A Thon:Thung Khru Bangkok: 02-470-8000");
+    }
+
+    public void Academia(){
+         Console.WriteLine("college name : name : ID card number");
+         Console.WriteLine("CU:MR.ChaiDi RakDi : 153-698-4561237");
+         Console.WriteLine("MU:MR.NonDi Chaidi  : 742-112-5687412");
+         Console.WriteLine("KMITL :MR.AphaiDi RianDi : 541-772-3215689");
+         Console.WriteLine("KKU :MR.ThamDi NganDi : 777-772-4578923");
+         Console.WriteLine("KMUTT :MR.Thon RiaBodi : 003-852-4752000");
+        }
+        public class ElectricCalculate{
+            flo
+        }
+    public void ElectricityFT(){
+        Console.WriteLine("min KKU: Watthana:30.2 ");
+        
+    }
+}
